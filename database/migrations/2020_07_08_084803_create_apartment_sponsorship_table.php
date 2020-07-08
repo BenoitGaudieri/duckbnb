979e0c1e53c250d50f6cd38b121bb7e302ae5312
BenoitGaudieri <benalek@gmail.com>
2020-07-08 11:13:08 +0200

    start project

---
+++ database/migrations/2020_07_08_084803_create_apartment_sponsorship_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentSponsorshipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_sponsorship', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_sponsorship');
+    }
+}
