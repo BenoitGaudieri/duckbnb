2f14e8b372c2295cc36bd33789af87a1d4934e87
Nicolo <nicolorizzi1996@gmail.com>
2020-07-09 17:24:36 +0200

    fix

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = 'user/home';
+    public const HOME = 'user/dashboard';
 
     /**
      * Define your route model bindings, pattern filters, etc.
