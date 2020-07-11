25db32d1abdaa0c8474e767197d3159d3ebe3ecf
Nicolo <nicolorizzi1996@gmail.com>
2020-07-11 10:37:31 +0200

    view fixing new input

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <h1 class="mb-4">New Post</h1>
+    <h1 class="mb-4">Crea un nuovo appartamento</h1>
 
     @if ($errors->any())
         <div class="alert alert-danger">
@@ -19,18 +19,53 @@
         @method("POST")
 
         <div class="form-group">
-            <label for="title">Title</label>
-            <input class="form-control" type="text" name="title" id="title" value="{{ old("title") }}">
-        </div>
-        <div class="form-group">
-            <label for="body">Body</label>
-            <textarea class="form-control" name="body" id="body">{{ old("body") }}</textarea>
+            <label for="title">Titolo :</label>
+            <input class="form-control" type="text" name="title" id="title" value="{{ old("title") }}" placeholder="Inserisci un titolo">
         </div>
 
         <div class="form-group">
-            <label for="path_img">Apartment image</label>
+            <label for="body">Descrizione :</label>
+            <textarea class="form-control" name="body" id="body" placeholder="Inserisci una descrizione">{{ old("body") }}</textarea>
+        </div>
+
+        <div class="form-group">
+            <label for="title">Prezzo :</label>
+            <input class="form-control" type="number" min="1" max="10000" name="price" id="price" value="{{ old("price") }}" placeholder="€">
+        </div>
+
+        <div class="form-group">
+            <label for="path_img">Apartment image :</label>
             <input class="form-control" type="file" name="path_img" id="path_img" accept="image/*">
         </div>
+
+        <div class="form-group">
+            <label for="room_qty">N° di stanze :</label>
+            <input class="form-control" type="number" min="1" max="10000" name="room_qty" id="room_qty" value="{{ old("room_qty") }}" placeholder="Inserisci il n° di stanze">
+        </div>
+
+        <div class="form-group">
+            <label for="bathroom_qty">N° di bagni :</label>
+            <input class="form-control" type="number" min="1" max="10000" name="bathroom_qty" id="bathroom_qty" value="{{ old("bathroom_qty") }}" placeholder="Inserisci il n° di bagni">
+        </div>
+
+        <div class="form-group">
+            <label for="bed_qty">N° di stanze :</label>
+            <input class="form-control" type="number" min="1" max="10000" name="bathroom_qty" id="bed_qty" value="{{ old("bathroom_qty") }}" placeholder="Inserisci il n° di stanze">
+        </div>
+
+        <div class="form-group">
+            <label for="sqr_meters">N° di m<sup>2</sup> :</label>
+            <input class="form-control" type="number" min="1" max="10000" name="bathroom_qty" id="bed_qty" value="{{ old("bathroom_qty") }}" placeholder="Inserisci il n° di metri quadri">
+        </div>
+
+        <label for="activeStatus">Active Status</label>
+        <select class="ml-1" id="activeStatus" name="activeStatus">
+            <option value="true" selected>Active</option>
+            <option value="false">Not Active</option>
+        </select>
+
+        <h1>+input algolia mappa</h1>
+
 
         <input class="btn btn-primary" type="submit" value="Create new post">
 
