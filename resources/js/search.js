4bb5b916b93f1aaa33cff54d8b9f307f119ecee0
BenoitGaudieri <benalek@gmail.com>
2020-07-13 12:37:58 +0200

    search view and logic

---
+++ resources/js/search.js
@@ -0,0 +1,45 @@
+const search = instantsearch({
+    indexName: "apartments",
+    searchClient: algoliasearch(
+        process.env.MIX_ALGOLIA_APP_ID,
+        process.env.MIX_ALGOLIA_SEARCH
+    )
+});
+
+search.addWidgets([
+    instantsearch.widgets.searchBox({
+        container: "#searchbox"
+    }),
+    instantsearch.widgets.clearRefinements({
+        container: "#clear-refinements"
+    }),
+    instantsearch.widgets.refinementList({
+        container: "#apartment-service",
+        attribute: "brand"
+    }),
+    instantsearch.widgets.hits({
+        container: "#hits",
+        templates: {
+            item: `
+          <div class="container">
+            <img src="/storage/{{ img_url }}" align="left" alt="{{title}}" />
+            <div class="hit-name">
+              {{#helpers.highlight}}{ "attribute": "title" }{{/helpers.highlight}}
+            </div>
+            <div class="hit-description">
+              {{#helpers.highlight}}{ "attribute": "description" }{{/helpers.highlight}}
+            </div>
+            <div class="item-lng">{{id}}</div>
+            <div class="hit-price">\${{price}}</div>
+            <div class="item-lat">{{lat}}</div>
+            <div class="item-lng">{{lng}}</div>
+          </div>
+        `
+        }
+    }),
+    instantsearch.widgets.pagination({
+        container: "#pagination"
+    })
+]);
+
+search.start();
--- resources/views/guests/search.blade.php
+++ resources/views/guests/search.blade.php
@@ -1,1 +1,40 @@
-<h1>SEARCH</h1>
+@extends('layouts.app')
+
+@section('content')
+
+<style>
+    img {
+        width: 100%;
+    }
+
+</style>
+
+<div class="ais-InstantSearch container">
+    <h1>Search</h1>
+
+    <div class="left-panel">
+      <div id="clear-refinements"></div>
+
+      <h2>Tags</h2>
+      <div id="apartment-service"></div>
+    </div>
+
+    <div class="right-panel">
+      <div id="searchbox" class="ais-SearchBox"></div>
+      <div id="hits"></div>
+      <div id="pagination"></div>
+    </div>
+  </div>
+
+  @endsection
+
+  @push('scripts')
+  <link
+  rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/instantsearch.css@7/themes/algolia-min.css"/>
+  <script src="https://cdn.jsdelivr.net/npm/algoliasearch@4/dist/algoliasearch-lite.umd.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/instantsearch.js@4"></script>
+  <script src="{{ asset("js/search.js") }}" defer></script>    
+
+      
+  @endpush
