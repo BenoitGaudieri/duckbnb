bb437fb28efa50e2d9fb273016073bc8179ec4a1
MattLeuterio <leuterio.matteo@gmail.com>
2020-07-13 10:33:05 +0200

    Counter Views, Fixed double header, name or email

--- resources/views/users/show.blade.php
+++ resources/views/users/show.blade.php
@@ -15,7 +15,7 @@
         <img src="{{ asset('storage/'. $apartment->img_url) }}" alt="{{ $apartment->title }}">
     </div>
     <div class="row">
-        {{ $apartment->views }}
+        Views: {{ $apartment->views }}
     </div>
     <div class="row">
         {{ $apartment->lat }}
