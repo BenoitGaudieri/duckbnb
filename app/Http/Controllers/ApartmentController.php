c08d7e1f157474c144fb3b5d166fa9b14b2c7cf6
Luigi <luigismilzo@live.com>
2020-07-13 17:22:31 +0200

    Show and validation in ApartmentController (user)

--- app/Http/Controllers/ApartmentController.php
+++ app/Http/Controllers/ApartmentController.php
@@ -1,10 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Apartment;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class ApartmentController extends Controller
 {
-    //
+    public function show(Apartment $apartment)
+    {
+        //salvare in db
+        if(Auth::id() <> $apartment['user_id']) {
+            $apartment['views'] = $apartment['views'] + 1;
+            $saved = $apartment->save();
+        }    
+        
+        return view("guests.show", compact("apartment"));
+    }
 }
