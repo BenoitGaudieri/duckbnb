350463d06688408fc6df113f09eb219a6016e4ba
BenoitGaudieri <benalek@gmail.com>
2020-07-15 20:55:17 +0200

    working solution

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -7,23 +7,27 @@ use App\Apartment;
 
 class SearchController extends Controller
 {
-    public function index() 
+    public function index()
     {
         return view('guests.search');
     }
-    
+
     public function search(Request $request)
     {
         $ids = $request->id[0];
         $array = explode(',', $ids);
 
-        $apartments = [];
+        $apartments = Apartment::all()->whereIn("id", $array);
 
-        foreach ($array as $item => $key) {
-            $apartments[] = Apartment::where('id', $key)->get();
-        }
-        
-        dd($apartments);
+
+        // for ($i = 0; $i < count($array); $i++) {
+        //     $selectedApts = $apartments->with(["id" => $array[$i]])->get();
+        // }
+
+        // foreach ($array as $item => $key) {
+        //     $apartments[] = Apartment::where('id', $key)->get();
+        // }
+
 
         return view('guests.search', compact('apartments'));
     }
--- resources/views/guests/search.blade.php
+++ resources/views/guests/search.blade.php
@@ -1,1 +1,19 @@
-@dd($apartments)
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container show-page">
+    @foreach ($apartments as $apartment)
+
+    <div class="card">
+        
+    </div>
+    
+    <h2>{{ $apartment->id }}</h2>
+    <h2>{{ $apartment->title }}</h2>
+
+    
+    @endforeach
+</div>
+
+@endsection
