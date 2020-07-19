76e2694825fb36226034db942c73812378c10048
MattLeuterio <leuterio.matteo@gmail.com>
2020-07-19 16:36:06 +0200

    sponsorship page finished and ready

--- resources/js/sponsorship.js
+++ resources/js/sponsorship.js
@@ -37,6 +37,7 @@ $(document).ready(function() {
     const amountInput = $('input#amount'); // Get Amount input
     const packInput = $('input#pack'); // Get Pack input
     const ctnCard = $('.pack-cards'); // Get Container Cards
+    const ctnPay = $('.payment'); // Get container Payment
 
     // Choice Section
     let choiceCtn = $('.choice'); // Get Container Choice
@@ -45,8 +46,11 @@ $(document).ready(function() {
 
     pack.click( function() {
         
-        // Show Choice Container
-        choiceCtn.show();
+        // Show/Hide Choice Container
+        choiceCtn.fadeToggle();
+
+        // Show/Hide payment container
+        ctnPay.fadeToggle();
 
         // Get color and container
         let bgColor = $(this).css('background-color');
@@ -54,9 +58,11 @@ $(document).ready(function() {
 
         // Get Price and duration
         let id = $(this).children('.pack-cards--single--id').text();
-        let price = $(this).children('.pack-cards--single--price').text();
+        let price = $(this).children('.pack-cards--single--price').children('.get-price').text();
         let duration = $(this).children('.pack-cards--single--duration').text();
         let cardNotSelected = $(this).siblings('.pk');
+
+        console.log(price, id);
 
         // Print in dom (Choice) price and duration
         choicePrice.text(price);
@@ -67,7 +73,7 @@ $(document).ready(function() {
 
         // Set Color
         ctnCard.css('background-color', bgColor);
-        
+
         // Set price 
         amountInput.val(price);
 
