6b8c3f27ff9daea590b478075ed9414d232b3375
Luigi <luigismilzo@live.com>
2020-07-19 13:39:26 +0200

    Added reset button functionality

--- resources/js/search-filtering.js
+++ resources/js/search-filtering.js
@@ -8,6 +8,17 @@ $(document).ready(function() {
         $('#search-results').html('');
         callApi(apartments);
     });
+    
+    $(document).on('change', 'select', function() {
+        $('#search-results').html('');
+        callApi(apartments);
+    });
+    
+    $(document).on('click', '#reset', function() {
+        $('#search-results').html('');
+        clearFilters();
+        resetCall(apartments);
+    });
 
 }); // <--- End Ready
 
@@ -76,4 +87,36 @@ function dataCompile(apartments) {
 
     console.log(data);
     return data;
+}
+
+function clearFilters() {
+    $('input:checkbox').each(function(){
+        var self = $(this);
+
+        self.prop('checked', false);
+    });
+    
+    $('select').each(function(){
+        var self = $(this);
+
+        self.val('1');
+    });
+}
+
+function resetCall(apartments) {
+    $.ajax({
+        url: 'http://127.0.0.1:8000/api/apartments/',
+        data: {
+            id: apartments,
+            services: 'all',
+            rooms: $('#select-rooms').val(),
+            beds: $('#select-beds').val()
+        },
+        success: function(res) {
+            console.log(res);
+        },
+        error: function() {
+            console.log('Errore');
+        }
+    });
 }
